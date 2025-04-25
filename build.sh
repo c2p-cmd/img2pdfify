469cc82d7ef10bb39c1d77179315520568e49999
Sharan Thakur <sharandt19@live.com>
2025-04-25 19:35:51 +0200

    done

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# This script sets up a virtual environment, installs dependencies, builds the package, and uploads it to PyPI.
-./venv/bin/pip install -r requirements.txt
-./venv/bin/python -m build
-./venv/bin/python -m twine upload dist/*
